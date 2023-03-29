4d7061f6eb8ae9eca188ff5be5ae233fcbce7e7e
Saidul Islam Santo <sksanto.pro@gmail.com>
2023-03-29 14:56:52 +0600

    project setup

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './components/App';
+import './styles/index.css';
 
 const element = document.getElementById('root');
 const root = ReactDOM.createRoot(element);
