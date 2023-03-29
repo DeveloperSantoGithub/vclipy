37edd76bc3ca08b01dfe8345e070b6571c3eb3d4
Saidul Islam Santo <sksanto.pro@gmail.com>
2023-03-29 15:45:55 +0600

    Search Bar styles

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -2,6 +2,12 @@ import React from 'react';
 import { BiSearch } from 'react-icons/bi';
 
 class SearchBar extends React.Component {
+	state = { keyword: '' };
+
+	handleChange = (event) => {
+		this.setState({ keyword: event.target.value });
+	};
+
 	render() {
 		return (
 			<div className="bg-gray-300 flex flex-row items-center justify-between w-full h-14 px-3">
@@ -9,9 +15,14 @@ class SearchBar extends React.Component {
 					<h2 className="font-semibold">Vclipy</h2>
 				</div>
 
-				<from className="bg-white rounded-3xl w-5/12 shadow-sm hover:shadow-md">
+				<from
+					onSubmit={''}
+					className="bg-white rounded-3xl w-5/12 shadow-sm hover:shadow-md"
+				>
 					<div className="flex items-center justify-center">
 						<input
+							value={this.state.keyword}
+							onChange={this.handleChange}
 							className="pl-4 text-xl  rounded-l-3xl outline-none py-2 w-11/12"
 							placeholder="Search"
 						/>
