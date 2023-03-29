572eb77b256321db1efd70e1d9d98ab49e274e57
Saidul Islam Santo <sksanto.pro@gmail.com>
2023-03-29 14:50:28 +0600

    project setup

---
+++ src/components/SearchBar.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+class SearchBar extends React.Component {
+	render() {
+		return (
+			<div>
+				<from>
+					<div>
+						<input />
+						<button></button>
+					</div>
+				</from>
+			</div>
+		);
+	}
+}
