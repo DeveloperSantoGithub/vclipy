607409e9ce62445a6a0db4c5dfc0c4d04a1e5e96
Saidul Islam Santo <sksanto.pro@gmail.com>
2023-03-29 15:42:00 +0600

    Search Bar styles

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -4,15 +4,27 @@ import { BiSearch } from 'react-icons/bi';
 class SearchBar extends React.Component {
 	render() {
 		return (
-			<div className="bg-blue-200 flex flex-row items-center justify-center w-11/12  ">
-				<from className="bg-white rounded-lg m-2 ">
-					<div>
-						<input className=" outline-none rounded-l-lg" />
-						<button className=" p-0">
-							<BiSearch className="mx-2 m-o p-0" />
+			<div className="bg-gray-300 flex flex-row items-center justify-between w-full h-14 px-3">
+				<div>
+					<h2 className="font-semibold">Vclipy</h2>
+				</div>
+
+				<from className="bg-white rounded-3xl w-5/12 shadow-sm hover:shadow-md">
+					<div className="flex items-center justify-center">
+						<input
+							className="pl-4 text-xl  rounded-l-3xl outline-none py-2 w-11/12"
+							placeholder="Search"
+						/>
+
+						<button className=" text-2xl p-2">
+							<BiSearch />
 						</button>
 					</div>
 				</from>
+
+				<div>
+					<h4>User</h4>
+				</div>
 			</div>
 		);
 	}
