947f7107b11fdfe4486904ac86db221a132dd118
Saidul Islam Santo <sksanto.pro@gmail.com>
2023-03-30 13:01:28 +0600

    video details components

--- src/components/VideoDetails.js
+++ src/components/VideoDetails.js
@@ -3,7 +3,14 @@ function VideoDetails({ video }) {
 		return <div>Loading...</div>;
 	}
 
-	return <div>{video.snippet.title}</div>;
+	return (
+		<div className="w-10/12">
+			<div className="flex flex-col justify-between gap-4">
+				<h4>{video.snippet.title}</h4>
+				<p>{video.snippet.description}</p>
+			</div>
+		</div>
+	);
 }
 
 export default VideoDetails;
