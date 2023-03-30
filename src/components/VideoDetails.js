6e4b6b6e4cb7c096ad1ff12653ac136b1fddd207
Saidul Islam Santo <sksanto.pro@gmail.com>
2023-03-30 13:35:17 +0600

    video details components

--- src/components/VideoDetails.js
+++ src/components/VideoDetails.js
@@ -3,8 +3,18 @@ function VideoDetails({ video }) {
 		return <div>Loading...</div>;
 	}
 
+	const videoSrc = `https://www.youtube.com/embed/${video.id.videoId}`;
+
 	return (
-		<div className=" flex items-center justify-center">
+		<div className=" flex flex-col items-center justify-center">
+			<div>
+				<iframe
+					className=" w-full aspect-auto"
+					src={videoSrc}
+					title={video.snippet.title}
+				/>
+			</div>
+
 			<div className="flex flex-col justify-between gap-4 w-9/12 p-3 my-2 border shadow-md rounded-md">
 				<h4 className="font-bold text-md">{video.snippet.title}</h4>
 				<p className="text-sm">{video.snippet.description}</p>
