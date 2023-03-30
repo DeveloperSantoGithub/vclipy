dd66279090771afb08faad0c8fd1c434b66f9278
Saidul Islam Santo <sksanto.pro@gmail.com>
2023-03-30 16:41:23 +0600

    Search bar Styling done

--- src/components/VideoItem.js
+++ src/components/VideoItem.js
@@ -1,23 +1,16 @@
 function VideoItem({ video, onVideoSelect }) {
 	return (
-		<div
-			onClick={() => onVideoSelect(video)}
-			className="flex w-9/12 items-center gap-5 my-2 border-b-2 pb-4 cursor-pointer"
-		>
-			<div>
-				<img
-					className="rounded-md max-w-full max-h-full object-cover"
-					src={video.snippet.thumbnails.medium.url}
-					alt="thumbnials"
-				/>
+		<div onClick={() => onVideoSelect(video)} className="video-item-container">
+			<div className="video-image">
+				<img src={video.snippet.thumbnails.medium.url} alt="thumbnials" />
 			</div>
 
-			<div className="flex flex-col justify-between h-36">
-				<h4>{video.snippet.title}</h4>
-				<h5>{video.snippet.channelTitle}</h5>
-				<p className="bg-gray-200 from-transparent p-2 rounded-md border-black ">
-					{video.snippet.description}
-				</p>
+			<div className="video-info">
+				<h4 className="title">{video.snippet.title}</h4>
+
+				<h5 className="channel-title">{video.snippet.channelTitle}</h5>
+
+				<p className="description">{video.snippet.description}</p>
 			</div>
 		</div>
 	);
