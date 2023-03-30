c4d4c0aac405c45168d98908c89e52d1d089d41e
Saidul Islam Santo <sksanto.pro@gmail.com>
2023-03-30 12:57:33 +0600

    video details components

--- src/components/VideoItem.js
+++ src/components/VideoItem.js
@@ -13,11 +13,11 @@ function VideoItem({ video, onVideoSelect }) {
 			</div>
 
 			<div className="flex flex-col justify-between h-36">
-				<div>{video.snippet.title}</div>
-				<div>{video.snippet.channelTitle}</div>
-				<div className="bg-gray-200 from-transparent p-2 rounded-md border-black ">
+				<h4>{video.snippet.title}</h4>
+				<h5>{video.snippet.channelTitle}</h5>
+				<p className="bg-gray-200 from-transparent p-2 rounded-md border-black ">
 					{video.snippet.description}
-				</div>
+				</p>
 			</div>
 		</div>
 	);
