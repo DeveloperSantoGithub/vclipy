c6598c31ef00e3d791ac88dfee04b6ecf140736e
Saidul Islam Santo <sksanto.pro@gmail.com>
2023-03-31 23:30:40 +0600

    bugs fix

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './components/App';
 import './styles/Global.scss';
-// import './styles/index.css';
 
 const element = document.getElementById('root');
 const root = ReactDOM.createRoot(element);
