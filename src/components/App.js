abad705cb9bcaf11336b2532ebd9847eda131aae
Saidul Islam Santo <sksanto.pro@gmail.com>
2023-03-31 23:33:25 +0600

    bugs fix

--- src/components/App.js
+++ src/components/App.js
@@ -25,7 +25,7 @@ class App extends Component {
 	render() {
 		return (
 			<>
-				<div className="flex items-center justify-center">
+				<div>
 					<SearchBar onSubmit={this.handleSearchSubmit} />
 				</div>
 
