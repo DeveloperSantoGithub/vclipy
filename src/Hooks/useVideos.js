23da6a1c9088a95f4f8c6169d3c7f823cc8a87b7
Saidul Islam Santo <sksanto.pro@gmail.com>
2023-04-10 16:00:58 +0600

    useVideos Hooks

--- src/Hooks/useVideos.js
+++ src/Hooks/useVideos.js
@@ -1,7 +1,20 @@
 // If you give me a list of -> inputs
 // I will give you a list of -> outputs
 
+import { useEffect, useState } from 'react';
+
 const useVideos = () => {
+	const [videos, setVideos] = useState([]);
+
+	useEffect(() => {
+		handleSearchSubmit('Figma');
+	}, []);
+
+	const handleSearchSubmit = async (keyword) => {
+		const result = await youtubeApi(keyword);
+
+		setVideos(result);
+	};
 	return;
 };
 
